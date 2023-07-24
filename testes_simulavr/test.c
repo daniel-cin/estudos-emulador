cc84ca1a5d7655110b99b22b6ae3c13767be4122
thiagoperroni <thiagomeletti23@gmail.com>
2023-07-24 19:25:52 -0300

    add support to printf-like debug

--- testes_simulavr/test.c
+++ testes_simulavr/test.c
@@ -1,20 +1,29 @@
 #include <avr/io.h>
+#include <stdio.h>
+
 
 /* This port corresponds to the "-W 0x20,-" command line option. */
 #define special_output_port (*((volatile char *)0x20))
 
+int virtual_putchar(char c, FILE *unused)
+{
+    special_output_port = c;
+    return 0;
+}
+
+FILE virtual_port = FDEV_SETUP_STREAM(virtual_putchar, NULL, _FDEV_SETUP_WRITE);
+
+
 void TIMER_start(void);
 uint16_t TIMER_stop(void);
 
-void UART_putc(unsigned char data);
-void UART_puts(unsigned char* s);
-void UART_putU16(uint16_t val);
-
 volatile uint8_t test = 0;
 
-void main(void)
+int main(void)
 {
     uint16_t val = 0;
+
+    stdout = &virtual_port;
 
     TIMER_start();
 
@@ -22,10 +31,9 @@ void main(void)
 
     val = TIMER_stop();
 
-    UART_puts("Cycles: ");
-    UART_putU16(val);
-    UART_putc('\r');
-    UART_putc('\n');
+    printf("Cycles: %u\r\n", val);
+
+    return 0;
 }
 
 void TIMER_start(void)
@@ -44,66 +52,4 @@ uint16_t TIMER_stop(void)
 
     /* Read Timer/Counter1 */
     return TCNT1;
-}
-
-void UART_putc(unsigned char data)
-{
-    special_output_port = data; /* Put data into buffer, sends the data */
-}
-
-void UART_puts(unsigned char* s)
-{
-    // transmit character until NULL is reached
-    while(*s > 0) UART_putc(*s++);
-}
-
-void UART_putU16(uint16_t val)
-{
-    uint8_t dig1 = '0', dig2 = '0', dig3 = '0', dig4 = '0';
-
-    // count value in 10000s place
-    while(val >= 10000)
-    {
-        val -= 10000;
-        dig1++;
-    }
-
-    // count value in 1000s place
-    while(val >= 1000)
-    {
-        val -= 1000;
-        dig2++;
-    }
-
-    // count value in 100s place
-    while(val >= 100)
-    {
-        val -= 100;
-        dig3++;
-    }
-
-    // count value in 10s place
-    while(val >= 10)
-    {
-        val -= 10;
-        dig4++;
-    }
-
-    // was previous value printed?
-    uint8_t prevPrinted = 0;
-
-    // print first digit (or ignore leading zeros)
-    if(dig1 != '0') {UART_putc(dig1); prevPrinted = 1;}
-
-    // print second digit (or ignore leading zeros)
-    if(prevPrinted || (dig2 != '0')) {UART_putc(dig2); prevPrinted = 1;}
-
-    // print third digit (or ignore leading zeros)
-    if(prevPrinted || (dig3 != '0')) {UART_putc(dig3); prevPrinted = 1;}
-
-    // print third digit (or ignore leading zeros)
-    if(prevPrinted || (dig4 != '0')) {UART_putc(dig4); prevPrinted = 1;}
-
-    // print final digit
-    UART_putc(val + '0');
 }
